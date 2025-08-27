8098ef6c49c2292eef1e8268dffd27502e080c49
Stephen Olaode <solaode@chlmd-solaode1.us.drwholdings.com>
2025-08-27 13:57:09 -0500

    Feature:Update2

--- buyside.py
+++ buyside.py
@@ -5,15 +5,16 @@ class Bank:
 
     def place_bet(self, amount):
         print(f"Confirm that you want to spend {amount} ")
-        if amount > starting_balance:
-            print("Insufficient funds, make a deposit in order to place further bets.")
+        if amount > self.balance:
+            print("Insufficient funds, make a deposit through the banker to place further bets.")
         else:
             self.balance -= amount
             self.current_bet = amount
-    def win()
-
-    def lose()
-
-    def moneyback()
-
-    def blackjack()
+    def win(self):
+        self.balance += self.current_bet
+        return f"You've won {self.current_bet}" 
+    def lose(self):
+        return f"You've lost {self.current_bet}"
+    def stand_off(self,):
+        return f"Stand-off, no payout"
+    def blackjack(self,):
