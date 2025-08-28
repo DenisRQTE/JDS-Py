b801c3e545a8c0e675a9c5626b2adedaf0d1cf5c
grimm00 <cdwilson001@@gmail.com>
2025-08-27 20:10:33 -0500

    refactor: Cards as objects, Score Keeping added
    The deck is built as a list of card objects, each having 3 attributes: suit, rank, and readbable text displayed to the player.
    
    - Variable "player_hand" starts as an empty list, awaiting more cards
    - New "deal" function pops last value from "deck" and adds it to player_hand list
    - Calculates total score based on cards in hand
    - Code is meant to promote object-oriented approach in the future for scalability

--- blackjack.py
+++ blackjack.py
@@ -14,27 +14,51 @@ import random
 stand = "stand"
 
 
-suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 
-card_values = {
+CARD_VALUES = {
     "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
     "J": 10, "Q": 10, "K": 10, "A": 11  
 }
 
-deck = [rank + " of " + suit for suit in suits for rank in ranks]
+deck = [{"suit": suit, 
+         "rank": rank, 
+         "text":"| " + rank + " of " + suit + " |" } 
+         for suit in SUITS for rank in RANKS]
 
-random.shuffle(deck)  
+random.shuffle(deck)
 
-print(deck[:2])
+def deal():
+    return deck.pop() 
 
-current_hand = deck[:-1]
-value = sum(card_values[card.split(" ")[0]] for card in current_hand)
+current_hand = []
+current_hand.append(deal())
+current_hand.append(deal())
+score = 0
+for card in current_hand:
+    print(card["text"], end=" ")
+print()
 
+for card in current_hand:
+    rank=card["rank"]
+    value=CARD_VALUES[rank]
+    if rank == "A":
+        flex_score=score
+        flex_score+=value
+        if flex_score > 21:
+            score+=1
+        else:
+            score=flex_score
+    else:
+        score+=value
+            
+print(f"Current score: {score}")
 
 hit = input("do you want to hit or stay : ")
 
+"""
 def hit_stay(hit):
     
     
@@ -66,8 +90,6 @@ def hit_stay(hit):
         print(value)
         return 
 
-hit_stay(hit)
+hit_stay(hit) 
 
-
-
-
+"""
